8448385c792eeec3267b38319de758ddb6442671
Manoel Brasil <manoelbo@hotmail.com>
2023-06-23 17:36:27 -0300

    meta token added to Verify Token call

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,17 @@
 const VerfiToken = (req, res) => {
-    res.send("hello"); 
+    try {
+        var accessToken = process.env.META_TOKEN;
+        var token = req.query("hub.verify_token");
+        var challenge = req.body["hub.challenge"];
+
+        if(challenge != null && token != null && token == accessToken) {
+            res.send(challenge);
+        } else {
+            res. status(400).send();
+        }
+    }catch(e){
+        res. status(400).send();
+    }
 }
 
 const ReceivedMessage = (req, res) => {
