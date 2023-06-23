b2de583c392034836bd338928d00f88f0e316b02
Manoel Brasil <manoelbo@hotmail.com>
2023-06-23 17:53:04 -0300

    fix

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,8 +1,8 @@
 const VerfiToken = (req, res) => {
     try {
         var accessToken = "EAAKZCbO5x9WcBANsC2Wc7M5wgX54o0wo3vMRYyybWZC83HRZBKGZCC88TMoXH7PErsKtSvAo2dd7J5dBXbHWGP0Azk76WIfWe5A1VMqePuPgvKGjkW8JHexdHnybbCdHj0oDIISZA6VJBHGsQoJDxMDz4pOsNB0qPsIwzEm70TWR2mkulbXbf";
-        var token = req.query("hub.verify_token");
-        var challenge = req.body["hub.challenge"];
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
 
         if(challenge != null && token != null && token == accessToken) {
             res.send(challenge);
