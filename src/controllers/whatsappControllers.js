330e2f7450e07073c13c4c26fe6e5474cc116e9e
Manoel Brasil <manoelbo@hotmail.com>
2023-06-23 17:47:10 -0300

    token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,6 +1,6 @@
 const VerfiToken = (req, res) => {
     try {
-        var accessToken = process.env.META_TOKEN;
+        var accessToken = "EAAKZCbO5x9WcBANsC2Wc7M5wgX54o0wo3vMRYyybWZC83HRZBKGZCC88TMoXH7PErsKtSvAo2dd7J5dBXbHWGP0Azk76WIfWe5A1VMqePuPgvKGjkW8JHexdHnybbCdHj0oDIISZA6VJBHGsQoJDxMDz4pOsNB0qPsIwzEm70TWR2mkulbXbf";
         var token = req.query("hub.verify_token");
         var challenge = req.body["hub.challenge"];
 
